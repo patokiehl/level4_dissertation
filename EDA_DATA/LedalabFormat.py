8089a3eaa46e18593fb7e2ff219b71bafd60e474
patokiehl <patrickkiehlmann@gmail.com>
2025-03-26 11:47:18 +0000

    updated credits for the LedalabFormat.py file

--- EDA_DATA/LedalabFormat.py
+++ EDA_DATA/LedalabFormat.py
@@ -1,30 +1,9 @@
 import os
 
-#current_dir = os.path.dirname(os.path.abspath(__file__))
+# Credit to Shaun Macdonald - edited to be able to work with multiple files
 current_dir = os.getcwd()
 input_folder = os.path.join(current_dir, "RAW_files")
 output_folder = os.path.join(current_dir, "formatted_files")
-
-
-# file = "RAW_files/participant8.txt"
-# GSR = []
-# f = open(file, 'r')
-# lines = f.readlines()[3:]
-# w = open("formatted_files/parsed" + file, "w")
-
-# for row in lines:
-#     row = row.rstrip()
-#     csvrow = row.split()
-#     signal = csvrow[5:]
-#     for i in signal:
-#         uS = ( ((float(i)/1024)*3.3)/0.12   )
-#         GSR.append(int(i))
-#         w.write(str(uS) + "\n")  
-
-# print (GSR[1:10])
-
-# w.close()
-
 
 def parse_file(in_file):
     out_file = os.path.join(output_folder, "parsed" + in_file)
